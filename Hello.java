bb16f1294deec3f34ec819d971b3b3ffbf81bcec
loomiskn <59926107+loomiskn@users.noreply.github.com>
2020-01-15 10:35:14 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/**********************************************
+* Java Hello
+***********************************************
+*Kingyata Loomis
+*January 15, 2020
+*cmsc 255 section 3
+**********************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+	  System.out.println("<Kingyata Loomis>");
+	  System.out.println("<Java Hello>");
+	  System.out.println("<First Lab>");
+	  System.out.println("<January, 15,2020>");
+	  System.out.println("cmsc 255 section 3>");
+	  System.out.println();
+	  }
+}
